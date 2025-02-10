c2e380536633bf90dc57b79b6f03e4b753dc6cd4
Emma <ewigur@gmail.com>
2025-02-10 09:06:25 +0100

    SceneCleanupMM

--- Assets/Scripts/MenuScripts/MainMenuButtons.cs
+++ Assets/Scripts/MenuScripts/MainMenuButtons.cs
@@ -1,14 +1,25 @@
-using System;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour
 {
-    [SerializeField] private Button button;
     [SerializeField] private GameObject menuPanel;
-    
-    
+    [SerializeField] private GameObject leaderBoardTable;
+
+    private void Awake()
+    {
+        if (menuPanel != null)
+        {
+            menuPanel.SetActive(true);
+        }
+        
+        if (leaderBoardTable != null)
+        {
+            leaderBoardTable.SetActive(false);
+        }
+    }
+
+
     public void onStartPressed()
     {
         SceneManager.LoadScene(1);
@@ -18,16 +29,18 @@ public class MainMenuButtons : MonoBehaviour
     {
         if(menuPanel.activeSelf)
             menuPanel.SetActive(false);
+        
+        leaderBoardTable.SetActive(true);
     }
 
     public void onSettingsPressed()
     {
-
+        Debug.Log("Settings was pressed");
     }
 
     public void onCreditsPressed()
     {
-
+        Debug.Log("Credits was pressed");
     }
 
     public void onQuitPressed()
@@ -38,6 +51,7 @@ public class MainMenuButtons : MonoBehaviour
 
     public void onReturnPressed()
     {
-
+        menuPanel.SetActive(true);
+        leaderBoardTable.SetActive(false);
     }
 }
