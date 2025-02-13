f7bc9efe6616d102de8dedfc99f114d8be79e701
Emma <ewigur@gmail.com>
2025-02-13 12:28:21 +0100

    UIFix

---
+++ Assets/Scripts/MenuScripts/UITimer.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class UITimer : MonoBehaviour
+{
+    private float delay = 4f;
+    private float fadeInDuration = 1f;
+    
+    [SerializeField] private CanvasGroup[] elementsToFadeIn; //Migh want to add other stuff in here, keep as array for now
+
+    private void Start()
+    {
+        foreach (var element in elementsToFadeIn)
+        {
+            element.alpha = 0f;
+            element.gameObject.SetActive(true);
+        }
+
+        StartCoroutine(FadeInUI());
+    }
+
+    private IEnumerator FadeInUI()
+    {
+        yield return new WaitForSeconds(delay);
+        
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeInDuration)
+        {
+            float alpha = Mathf.Lerp(0f, 1f, elapsedTime / fadeInDuration);
+
+            foreach (var element in elementsToFadeIn)
+            {
+                element.alpha = alpha;
+            }
+            
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        foreach (var element in elementsToFadeIn)
+        {
+            element.alpha = 1f;
+        }
+    }
+}
